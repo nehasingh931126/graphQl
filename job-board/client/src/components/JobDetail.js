d2b13c383b1b196f212df7bc0f67da64aa7f8911
Neha Singh <neha.singh931126@gmail.com>
2023-03-17 01:35:52 +0530

    Adding the error messagee for the code

--- job-board/client/src/components/JobDetail.js
+++ job-board/client/src/components/JobDetail.js
@@ -4,17 +4,24 @@ import { useState, useEffect } from 'react';
 import { getJob } from '../graphql/queries';
 function JobDetail() {
   const [job, setJob] = useState(null);
+  const [error, setError] = useState('');
   const { jobId } = useParams();
 
   useEffect(()=> {
     getJob(jobId).then((job)=> {
       console.log(job)
       setJob(job);
+    }).catch((error)=> {
+      setError(error);
     });
   }, [jobId]);
-  
+
   if(!job) {
     return <p>Loading</p>
+  }
+
+  if(!error) {
+    return <p>Something went wrong</p>
   }
   return (
     <div>
